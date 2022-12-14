496843de6c2a40d9ff7928ddd825161092f1b868
Cinta-Ikhlas <92847573+Cinta-Ikhlas@users.noreply.github.com>
2022-12-14 16:27:10 +0700

    First Commit

---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,73 @@
+@include('partials.header', ['title' => 'Register Page - Kelompok 1'])
+
+<body>
+    <div class="app app-auth-sign-up align-content-stretch d-flex flex-wrap justify-content-end">
+        <div class="app-auth-background"></div>
+        <div class="app-auth-container">
+            <div class="logo">
+                <a href="{{ url('/') }}">Kelompok 1</a>
+            </div>
+            <p class="auth-description">
+                Please enter your credentials to create an account.<br />Already have
+                an account? <a href="{{ url('/login') }}">Sign In</a>
+            </p>
+
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="{{ url('/register') }}" method="POST">
+                @csrf
+                <div class="auth-credentials m-b-xxl">
+                    <label for="signUpUsername" class="form-label">Full Name</label>
+                    <input type="text" name="name" class="form-control m-b-md" id="signUpUsername"
+                        aria-describedby="signUpUsername" placeholder="Enter your name" />
+
+                    <label for="signUpEmail" class="form-label">Email address</label>
+                    <input type="email" name="email" class="form-control m-b-md" id="signUpEmail"
+                        aria-describedby="signUpEmail" placeholder="example@neptune.com" />
+
+                    <label for="signUpPassword" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="signUpPassword"
+                        aria-describedby="signUpPassword"
+                        placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
+                    <div id="emailHelp" class="form-text">
+                        Password must be minimum 8 characters length*
+                    </div>
+
+
+                    {!! RecaptchaV3::field('register') !!}
+                </div>
+
+                <div class="auth-submit">
+                    <button type="submit" class="btn btn-primary">Sign Up</button>
+                </div>
+            </form>
+
+
+            <div class="divider"></div>
+            <div class="auth-alts">
+            </div>
+        </div>
+    </div>
+
+    @include('partials.footer')
+</body>
